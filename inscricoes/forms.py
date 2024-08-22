e6042d5e4812b919a4b7ed65c4285e539330e7e4
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2024-08-22 14:50:25 -0300

    Atualização do sistema

--- inscricoes/forms.py
+++ inscricoes/forms.py
@@ -1,19 +1,28 @@
-# forms.py na app "inscricoes"
-
 from django import forms
 from .models import Inscricao
+from captcha.fields import CaptchaField
 
 class InscricaoForm(forms.ModelForm):
+    confirmacao = forms.BooleanField(label="Eu confirmo que as informações fornecidas estão corretas", required=True)
+    captcha = CaptchaField(label='Por favor, verifique que você não é um robô')
+
     class Meta:
         model = Inscricao
         fields = [
             'nome_completo',
             'nome_responsavel',
+            'cpf',
+            'data_nascimento',
             'email',
-            'telefone',
             'endereco',
+            'cidade',
+            'uf',
+            'telefone',
             'irmaos',
             'unidade',
             'nivel',
-            'motivo_bolsa'
+            'escola',
+            'selecao',
+            'confirmacao',
+            'captcha'
         ]
--- inscricoes/models.py
+++ inscricoes/models.py
@@ -1,14 +1,17 @@
-# models.py na app "inscricoes"
 from django.db import models
 
 class Inscricao(models.Model):
-    nome_completo = models.CharField(verbose_name='Nome Completo do Candidato:', max_length=255)
-    nome_responsavel = models.CharField(verbose_name='Nome do Resposavel:', max_length=255)
+    nome_completo = models.CharField(verbose_name='Nome Completo do Candidato:', max_length=255, blank=True)
+    nome_responsavel = models.CharField(verbose_name='Nome do Responsável:', max_length=255, blank=True)
+    cpf = models.CharField(verbose_name='CPF', max_length=11, unique=True, default='00000000000')
+    data_nascimento = models.DateField(verbose_name='Data de Nascimento', blank=True, null=True)  # null=True adicionado
     email = models.EmailField()
-    telefone = models.CharField(verbose_name='Telefone para contado:', max_length=15)
-    endereco = models.TextField(verbose_name='Endereço:', blank=True)
+    endereco = models.CharField(verbose_name='Endereço:', max_length=255, blank=True)  # max_length adicionado
+    cidade = models.CharField(verbose_name='Cidade:', max_length=255, blank=True)  # max_length adicionado
+    uf = models.CharField(verbose_name='UF:', max_length=2, blank=True)  # max_length adicionado e alterado para CharField
+    telefone = models.CharField(verbose_name='Telefone para contado:', max_length=15, blank=True)
     irmaos = models.CharField(verbose_name='Tem irmãos?', max_length=20, choices=[('Não', 'Não Tenho'),('Um', 'Um'), ('Dois', 'Dois'), ('Três ou Mais', 'Três ou Mais')])
-    unidade = models.CharField(max_length=50, choices=[('Primavera', 'Unidade Primavera'), ('Bela Vista', 'Unidade Bela Vista')])
+    unidade = models.CharField(max_length=50, choices=[('Primavera', 'Unidade Primavera'), ('Bela Vista', 'Unidade Bela Vista')], blank=True)
     nivel = models.CharField(max_length=50, choices=[
         ('Maternal Babay', 'Maternal Babay'),
         ('1º ano Fundamental I', '1º ano Fundamental I'),
@@ -23,9 +26,20 @@ class Inscricao(models.Model):
         ('1ª Série do Ensino Médio', '1ª Série do Ensino Médio'),
         ('2ª Série do Ensino Médio', '2ª Série do Ensino Médio'),
         ('3ª Série do Ensino Médio', '3ª Série do Ensino Médio')
-
     ])
-    motivo_bolsa = models.TextField()
+    escola = models.CharField(verbose_name='Escola que já estudou', max_length=255, default='Escola Padrão')
+    selecao = models.CharField(max_length=50, choices=[
+        ('Instagram', 'Instagram'),
+        ('Facebook', 'Facebook'),
+        ('Twitter', 'Twitter'),
+        ('YouTube', 'YouTube'),
+        ('Grupos Whatsapp', 'Grupos Whatsapp'),
+        ('Coordenação / Professores', 'Coordenação / Professores'),
+        ('Pais', 'Pais'),
+        ('Alunos', 'Alunos')
+    ], default='Não informado')
+
+    confirmacao = models.BooleanField(verbose_name='Eu confirmo que as informações fornecidas estão corretas', default=False)
 
     def __str__(self):
         return self.nome_completo
