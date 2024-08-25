7a22629da06b6d96d99ae0a8e71dfd261e72de4e
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2024-08-25 09:35:08 -0300

    migração

--- inscricoes/models.py
+++ inscricoes/models.py
@@ -3,16 +3,16 @@ from django.db import models
 class Inscricao(models.Model):
     nome_completo = models.CharField(verbose_name='Nome Completo do Candidato:', max_length=255, blank=True)
     nome_responsavel = models.CharField(verbose_name='Nome do Responsável:', max_length=255, blank=True)
-    cpf = models.CharField(verbose_name='CPF', max_length=11, unique=True, default='00000000000')
+    cpf = models.CharField(verbose_name='CPF', max_length=11, unique=True, blank=True, null=True)
     data_nascimento = models.DateField(verbose_name='Data de Nascimento', blank=True, null=True)
     email = models.EmailField()
     endereco = models.CharField(verbose_name='Endereço:', max_length=255, blank=True)
     cidade = models.CharField(verbose_name='Cidade:', max_length=255, blank=True)
     uf = models.CharField(verbose_name='UF:', max_length=2, blank=True)
     telefone = models.CharField(verbose_name='Telefone para contato:', max_length=15, blank=True)
-    irmaos = models.CharField(verbose_name='Tem irmãos?', max_length=20, choices=[('Não', 'Não Tenho'),('Um', 'Um'), ('Dois', 'Dois'), ('Três ou Mais', 'Três ou Mais')])
+    irmaos = models.CharField(verbose_name='Tem irmãos?', max_length=20, blank=True, null=True, choices=[('Não', 'Não Tenho'),('Um', 'Um'), ('Dois', 'Dois'), ('Três ou Mais', 'Três ou Mais')])
     unidade = models.CharField(max_length=50, choices=[('Primavera', 'Unidade Primavera'), ('Bela Vista', 'Unidade Bela Vista')], blank=True)
-    nivel = models.CharField(max_length=50, choices=[
+    nivel = models.CharField(max_length=50, blank=True, null=True, choices=[
         ('Maternal Babay', 'Maternal Babay'),
         ('1º ano Fundamental I', '1º ano Fundamental I'),
         ('2º ano Fundamental I', '2º ano Fundamental I'),
@@ -27,8 +27,8 @@ class Inscricao(models.Model):
         ('2ª Série do Ensino Médio', '2ª Série do Ensino Médio'),
         ('3ª Série do Ensino Médio', '3ª Série do Ensino Médio')
     ])
-    escola = models.CharField(verbose_name='Escola que já estudou', max_length=255, default='Escola Padrão')
-    selecao = models.CharField(max_length=50, choices=[
+    escola = models.CharField(verbose_name='Escola que já estudou', max_length=255, blank=True, null=True)
+    selecao = models.CharField(max_length=50, blank=True, null=True, choices=[
         ('Instagram', 'Instagram'),
         ('Facebook', 'Facebook'),
         ('Twitter', 'Twitter'),
@@ -38,7 +38,7 @@ class Inscricao(models.Model):
         ('Pais', 'Pais'),
         ('Alunos', 'Alunos')
     ], default='Não informado')
-    confirmacao = models.BooleanField(verbose_name='Eu confirmo que as informações fornecidas estão corretas', default=False)
+    confirmacao = models.BooleanField(verbose_name='Eu confirmo que as informações fornecidas estão corretas', blank=True, null=True)
 
     def __str__(self):
         return self.nome_completo
