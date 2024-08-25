a4e19cdb46532d9e0b2645f83ea417f8ffcb3c12
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2024-08-25 09:59:31 -0300

    subindo

--- inscricoes/urls.py
+++ inscricoes/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('download_excel/', download_excel, name='download_excel'),  # Rota para download do Excel
     path('lista_inscritos/', lista_inscritos, name='lista_inscritos'),  # Rota para listar inscritos
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),  # Página de login
-    path('logout/', auth_views.LogoutView.as_view(next_page='inscricao'), name='logout'),  # Rota para logout
+    path('logout/', auth_views.LogoutView.as_view(next_page=''), name='logout'),  # Rota para logout
 ]
